c5d47464c9a5b0cfd52a101fe6d9c57bd4acc8ee
Corvin <307276741+Corvinus2991@users.noreply.github.com>
2026-07-21 14:00:45 +0200

    Add files via upload

---
+++ layout.js
@@ -0,0 +1,9 @@
+import ChefMapApp from "../components/ChefMapApp";
+
+export default function Home() {
+  return (
+    <main style={{ padding: "1.5rem 1rem" }}>
+      <ChefMapApp />
+    </main>
+  );
+}
