02c9aa022813348e852bd055eabcbdf52d780931
Corvin <307276741+Corvinus2991@users.noreply.github.com>
2026-07-21 15:30:42 +0200

    Delete layout.js

--- layout.js
+++
@@ -1,9 +0,0 @@
-import ChefMapApp from "../components/ChefMapApp";
-
-export default function Home() {
-  return (
-    <main style={{ padding: "1.5rem 1rem" }}>
-      <ChefMapApp />
-    </main>
-  );
-}
